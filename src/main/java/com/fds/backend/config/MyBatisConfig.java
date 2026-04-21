e027c86e1d0cc05b266ecf082d2e3512be68b3a3
tuanla <tuanla@vnpost.vn>
2026-04-21 18:06:34 +0700

    update

--- src/main/java/com/fds/backend/config/MyBatisConfig.java
+++ src/main/java/com/fds/backend/config/MyBatisConfig.java
@@ -1,9 +1,10 @@
 package com.fds.backend.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.fds.backend")
+@MapperScan(basePackages = "com.fds.backend", annotationClass = Mapper.class)
 public class MyBatisConfig {
 }
